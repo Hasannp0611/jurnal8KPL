125d26ed361db5f28790a38546e146ca80907508
Hasannp0611 <hasannp0611@gmail.com>
2024-04-20 09:01:21 +0700

    Aplikasi Bank Transfer dengan Runtime Configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace modul8_1302220031
+{
+	public class BankTransferConfig
+	{
+        public BankTransferConfig loadData()
+		{
+            string data = File.ReadAllText("/Users/hasannp/SEMESTER (4)/5. Konstruksi PL/modul8_1302220031/bank_transfer_config.json");
+            BankTransferConfig json = JsonConvert.DeserializeObject<BankTransferConfig>(data);
+            return json;
+        }
+
+        public void UbahBahasa()
+        {
+            if (lang == "en")
+            {
+                lang = "id";
+            }
+            else if (lang == "id")
+            {
+                lang = "en";
+            }
+        }
+
+        public string lang { get; set; }
+        public int transfer { get; set; }
+
+        public string methods { get; set; }
+        public string confirmation { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using modul8_1302220031;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig().loadData();
+
+        Console.WriteLine(config.lang);
+        Console.WriteLine("Please insert the amount of money to transfer:");
+        int money = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        if (config.transfer != 0)
+        {
+            if (uang <= 25000000)
+            {
+                Console.WriteLine("6500");
+            }
+            else
+            {
+                Console.WriteLine("15000");
+            }
+        }
+
+        Console.WriteLine("Select transfer method:" + "\n");
+        Console.WriteLine("1. RTO(real-time)");
+        Console.WriteLine("2. SKN");
+        Console.WriteLine("3. RTGS");
+        Console.WriteLine("4. BI FAST");
+        int methods = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n" + "Please type " + config.confirmation + " to confirm the transaction");
+        string confirm = Console.ReadLine();
+        if (confirm == "yes")
+        {
+            Console.WriteLine("Transfer is completed");
+        }
+        else {
+            Console.WriteLine("Transfer is cancelled");
+        }
+
+
+        //Merubah Menjadi Bahasa Indonesia
+        config.UbahBahasa();
+
+
+        Console.WriteLine(config.lang);
+        Console.WriteLine("Masukan jumlah uang yang akan di-transfer:");
+        int uang = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        if (config.transfer != 0)
+        {
+            if (uang <= 25000000)
+            {
+                Console.WriteLine("6500");
+            }
+            else
+            {
+                Console.WriteLine("15000");
+            }
+        }
+
+        Console.WriteLine("Pilih metode transfer:" + "\n");
+        Console.WriteLine("1. RTO(real-time)");
+        Console.WriteLine("2. SKN");
+        Console.WriteLine("3. RTGS");
+        Console.WriteLine("4. BI FAST");
+        int metode = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n" + "Ketik " + config.confirmation + " untuk mengonfirmasi transaksi");
+        string konfirmasi = Console.ReadLine();
+        if (confirm == "ya")
+        {
+            Console.WriteLine("Proses transfer berhasil");
+        }
+        else
+        {
+            Console.WriteLine("Transfer dibatalkan");
+        }
+    }
+}
